1655eebc31a29113e9d80a330cc008a1244f9476
nguyen-tho <nguyencongtho116@gmail.com>
2025-11-08 11:40:42 +0700

    add agent tools

---
+++ common_agent/agent.py
@@ -0,0 +1,61 @@
+from google.adk.agents.llm_agent import Agent
+import common_agent.AgentTools.timezone as tz_module
+import common_agent.AgentTools.location as loc_module
+
+def get_current_time(city_name: str) -> str:
+    """
+    Get the current time in the specified city.
+
+    Args:
+        city_name (str): The name of the city.
+    Returns:
+        str: The current time in the specified city formatted as "YYYY-MM-DD HH:MM:SS".
+    """
+    try:
+        location = tz_module.TimeZoneLocation(city_name)
+        current_time = location.get_current_time_in_city()
+        return {"status": "success", "city": city_name, "time": current_time}
+    except ValueError as e:
+        return {"status": "error", "message": str(e)}
+
+def get_location_coordinates(city_name: str, to_dms: bool) -> tuple:
+    """
+    Get the latitude and longitude of the specified city.
+
+    Args:
+        city_name (str): The name of the city.
+    Returns:
+        tuple: A tuple containing the latitude and longitude of the city.
+    """
+    try:
+        location = loc_module.Location(city_name)
+        lat, lon = location.get_location_based_on_city()
+        if to_dms:
+            lat = location.float_to_DMS(lat, is_latitude=True)
+            lon = location.float_to_DMS(lon, is_latitude=False)
+        return {"status": "success", "city": city_name, "coordinates": {"latitude": lat, "longitude": lon}}
+    except ValueError as e:
+        return {"status": "error", "message": str(e)}
+def get_location_timezone(city_name: str) -> str:
+    """
+    Get the timezone of the specified city.
+
+    Args:
+        city_name (str): The name of the city.
+    Returns:
+        str: The timezone of the specified city.
+    """
+    try:
+        location = tz_module.TimeZoneLocation(city_name)
+        timezone_str = location.get_timezone_based_on_city()
+        return {"status": "success", "city": city_name, "timezone": timezone_str}
+    except ValueError as e:
+        return {"status": "error", "message": str(e)}
+
+root_agent = Agent(
+    model='gemini-2.5-flash',
+    name='root_agent',
+    description="Tells the current time in a specified city.",
+    instruction="You are a helpful assistant that tells the current time, location coordinate, and timezone in cities.",
+    tools=[get_current_time, get_location_coordinates, get_location_timezone],
+)
